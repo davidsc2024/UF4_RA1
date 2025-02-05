d327cdaa33da9c09d98133234512ac75498f7e40
davidsc2024 <2024_davids.carbajal@iticbcn.cat>
2025-02-05 18:19:12 +0100

    Inicio practica buscador pokemon

---
+++ PokeAPI/index.js
@@ -0,0 +1,27 @@
+const URL_POKEMON = "https://pokeapi.co/api/v2/pokemon/squirtle";
+
+async function getImg(){
+  const dada = await resposta.json();
+
+  const nom
+
+}
+
+/*console.log("Línia 11");
+fetch(URL_POKEMON)
+  .then(function(resposta) {
+    console.log("Línia 22");
+    return resposta.json(); 
+  })
+  .then(function(dada) {
+    console.log("Línia 33");
+    console.log(dada.sprites.front_default); 
+    let srcImage = dada.sprites.front_default; 
+    document.getElementById("pokemon-image").src = srcImage;
+    document.getElementById("pokemon-name") = 
+  })
+  .catch(function(error) {
+    console.error("Error al hacer la consulta:", error);
+  });
+
+console.log("Línia 44");*/
